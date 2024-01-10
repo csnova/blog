d385d8d50eefc71c8f07efb8b82b7cc95ce3eadd
Chandler Nova <chandlersnova@gmail.com>
2024-01-10 11:35:28 -0800

    Skelton React Admin Frontend

---
+++ admin_frontend/src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "../styles/Home.css";
+
+function Home() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div></div>
+      <h1>Blog Admin</h1>
+    </>
+  );
+}
+
+export default Home;
